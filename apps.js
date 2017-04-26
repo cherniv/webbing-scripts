e03dac3971f4a29c07ef9fe33f195ea202b55b2e
Ivan <cherniv@gmail.com>
2017-04-26 10:49:45 +0300

    apps list includes code-push keys

--- apps.js
+++ apps.js
@@ -4,11 +4,59 @@ module.exports = [
 	},
 	{
 		'folder': 'WebbingMobileReact',
+		'code-push': {
+			'ios': {
+				'id': 'WebbingIos',
+				'keys': {
+					'production': '',
+					'staging': '',
+				}
+			},
+			'android': {
+				'id': 'WebbingAndroid',
+				'keys': {
+					'production': '',
+					'staging': '',
+				}
+			}
+		}
 	},
 	{
 		'folder': 'MiriamWell',
+		'code-push': {
+			'ios': {
+				'id': 'BaerMiriamIos',
+				'keys': {
+					'production': '',
+					'staging': '',
+				}
+			},
+			'android': {
+				'id': 'BaerMiriamAndroid',
+				'keys': {
+					'production': '',
+					'staging': '',
+				}
+			}
+		}
 	},
 	{
 		'folder': 'iomv',
+		'code-push': {
+			'ios': {
+				'id': 'IomvIos',
+				'keys': {
+					'production': '',
+					'staging': '',
+				}
+			},
+			'android': {
+				'id': 'IomvAndroid',
+				'keys': {
+					'production': '',
+					'staging': '',
+				}
+			}
+		}
 	},
 ]
